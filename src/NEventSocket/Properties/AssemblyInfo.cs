5fb76bec61765ce8c1aa54ec77b84b3455f0d1ac
Dan Barua <danbarua@gmail.com>
2013-10-17 16:28:03 +0100

    Hello World

---
+++ src/NEventSocket/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Business Systems (UK) Ltd">
+//   Copyright © Business Systems (UK) Ltd and contributors. All rights reserved.
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NEventSocket")]
+[assembly: AssemblyDescription("A library for building telephony applications with FreeSwitch")]
+[assembly: ComVisible(false)]
---
+++ test/NEventSocket.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Business Systems (UK) Ltd">
+//   Copyright © Business Systems (UK) Ltd and contributors. All rights reserved.
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NEventSocket.Specs")]
+[assembly: AssemblyDescription("")] 
+[assembly: ComVisible(false)]
