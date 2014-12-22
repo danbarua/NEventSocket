747046745db2326dd5d6fde153455a0b78e86b00
Dan Barua <danbarua@gmail.com>
2014-12-22 15:36:35 +0000

    Invert if for readability

--- src/NEventSocket/FreeSwitch/EventMessage.cs
+++ src/NEventSocket/FreeSwitch/EventMessage.cs
@@ -1,7 +1,6 @@
 ﻿namespace NEventSocket.FreeSwitch
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
@@ -24,64 +23,60 @@
 
         public EventMessage(BasicMessage basicMessage)
         {
-            if (basicMessage.ContentType == ContentTypes.EventPlain)
-            {
-                // normally, the content of the event will be in the BasicMessage's body text and will need to be parsed to produce an EventMessage
-
-                if (string.IsNullOrEmpty(basicMessage.BodyText)) throw new ArgumentException("Message did not contain an event body.");
-
-                try
-                {
-                    if (!basicMessage.BodyText.Contains(HeaderNames.ContentLength))
-                    {
-                        //body text consists of key-value-pair event headers
-                        this.Headers = basicMessage.BodyText.ParseKeyValuePairs("\n", ": ");
-                        this.BodyText = null;
-                    }
-                    else
-                    {
-                        //...but some Event Messages also carry a body payload, eg. a BACKGROUND_JOB event
-                        // which is a message carried inside an EventMessage carried inside a BasicMessage...
-
-                        //todo: this is really inefficient but a quick lazy way of turning a string into a message.
-                        var parser = new Parser();
-                        foreach (char c in basicMessage.BodyText)
-                        {
-                            parser.Append(c);
-                        }
-
-                        BasicMessage payload = parser.ExtractMessage();
-
-                        this.Headers = payload.Headers;
-                        this.BodyText = payload.BodyText.Trim();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Log.ErrorException("Failed to parse body of event", ex);
-                    Log.Error(this.BodyText);
-                    throw;
-                }
-            }
-            else
+            if (basicMessage.ContentType != ContentTypes.EventPlain)
             {
                 /* 
                  * Special Case:
-                 * When an Outbound Socket sends the "connect" command, FreeSwitch replies
-                 * with a Command-Reply. This Command-Reply message contains a CHANNEL_DATA event message in its headers.
-                 * In this case, a Command-Reply message also functions as an Event message, so we'll just copy the properties over
-                 * and return an EventMessage.
+                 * When an Outbound Socket sends the "connect" command, FreeSwitch responds with a Command-Reply.
+                 * This Command-Reply message contains a CHANNEL_DATA event message buried in its headers.
+                 * In this case, we can hydrate an event message from a message of content type Command-Reply.
                  */
 
                 if (basicMessage.Headers.ContainsKey(HeaderNames.EventName))
                 {
                     this.Headers = basicMessage.Headers;
                     this.BodyText = basicMessage.BodyText;
+                    return;
+                }
+
+                /* otherwise, we'll throw an exception if we get the wrong content-type message passed in*/
+                throw new InvalidOperationException("Expected content type event/plain, got {0} instead.".Fmt(basicMessage.ContentType));
+            }
+
+            // normally, the content of the event will be in the BasicMessage's body text and will need to be parsed to produce an EventMessage
+            if (string.IsNullOrEmpty(basicMessage.BodyText)) throw new ArgumentException("Message did not contain an event body.");
+
+            try
+            {
+                if (!basicMessage.BodyText.Contains(HeaderNames.ContentLength))
+                {
+                    //body text consists of key-value-pair event headers
+                    this.Headers = basicMessage.BodyText.ParseKeyValuePairs("\n", ": ");
+                    this.BodyText = null;
                 }
                 else
                 {
-                    throw new ArgumentException("Expected content type event/plain, got {0} instead.".Fmt(basicMessage.ContentType));
+                    //...but some Event Messages also carry a body payload, eg. a BACKGROUND_JOB event
+                    // which is a message carried inside an EventMessage carried inside a BasicMessage..
+                    // yo dawg, i heard you like messages...
+
+                    var parser = new Parser();
+                    foreach (char c in basicMessage.BodyText)
+                    {
+                        parser.Append(c);
+                    }
+
+                    BasicMessage payload = parser.ExtractMessage();
+
+                    this.Headers = payload.Headers;
+                    this.BodyText = payload.BodyText.Trim();
                 }
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException("Failed to parse body of event", ex);
+                Log.Error(this.BodyText);
+                throw;
             }
         }
 
