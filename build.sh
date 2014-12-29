a782300c553c2c044c2a1ae08732c9fe0bc37715
Dan Barua <danbarua@gmail.com>
2014-12-29 12:27:27 +0000

    Add mono build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+mono --runtime=v4.0 .nuget/NuGet.exe install .nuget/packages.config -OutputDirectory packages
+mono --runtime=v4.0 .nuget/NuGet.exe restore NEventSocket.sln
+xbuild NEventSocket.sln /verbosity:minimal
+mono --runtime=v4.0 ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe test/NEventSocket.Tests/bin/Debug/NEventSocket.Tests.dll
