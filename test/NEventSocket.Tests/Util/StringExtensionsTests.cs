df87deb49e7aa800089c32f1dd0f99738b10a5d1
Dan Barua <danbarua@gmail.com>
2016-03-16 16:56:58 +0000

    Add Digits for converting numbers to file strings
    Rationale: when interrupting hold music with queue position,
    if you use .PlayFile() | .Say() | .PlayFile()
    then there is a pause in between each app execution in which
    the hold-music leaks through.
    This can be used to build up a file_string input to the playback
    application which can be used for seamless playback over hold music.

--- test/NEventSocket.Tests/Util/StringExtensionsTests.cs
+++ test/NEventSocket.Tests/Util/StringExtensionsTests.cs
@@ -11,10 +11,13 @@ namespace NEventSocket.Tests.Util
 {
     using System;
 
+    using FluentAssertions;
+
     using NEventSocket.FreeSwitch;
     using NEventSocket.Util;
-
+    
     using Xunit;
+    using Xunit.Extensions;
 
     public class StringExtensionsTests
     {
@@ -64,6 +67,51 @@ namespace NEventSocket.Tests.Util
         {
             const string Input = "THIS_IS_AN_INVALID_HANGUPCAUSE";
             Assert.Throws<ArgumentException>(() => Input.HeaderToEnum<HangupCause>());
+        }
+
+        [Theory]
+        [InlineData(0, "digits/0.wav")]
+        [InlineData(1, "digits/1.wav")]
+        [InlineData(2, "digits/2.wav")]
+        [InlineData(10, "digits/10.wav")]
+        [InlineData(11, "digits/11.wav")]
+        [InlineData(12, "digits/12.wav")]
+        [InlineData(20, "digits/20.wav")]
+        [InlineData(23, "digits/20.wav!digits/3.wav")]
+        [InlineData(36, "digits/30.wav!digits/6.wav")]
+        [InlineData(100, "digits/1.wav!digits/hundred.wav")]
+        [InlineData(110, "digits/1.wav!digits/hundred.wav!digits/10.wav")]
+        [InlineData(116, "digits/1.wav!digits/hundred.wav!digits/16.wav")]
+        [InlineData(123, "digits/1.wav!digits/hundred.wav!digits/20.wav!digits/3.wav")]
+        [InlineData(199, "digits/1.wav!digits/hundred.wav!digits/90.wav!digits/9.wav")]
+        [InlineData(1000, "digits/1.wav!digits/thousand.wav")]
+        [InlineData(1005, "digits/1.wav!digits/thousand.wav!digits/5.wav")]
+        [InlineData(1010, "digits/1.wav!digits/thousand.wav!digits/10.wav")]
+        [InlineData(1016, "digits/1.wav!digits/thousand.wav!digits/16.wav")]
+        [InlineData(1023, "digits/1.wav!digits/thousand.wav!digits/20.wav!digits/3.wav")]
+        [InlineData(1099, "digits/1.wav!digits/thousand.wav!digits/90.wav!digits/9.wav")]
+        [InlineData(1200, "digits/1.wav!digits/thousand.wav!digits/2.wav!digits/hundred.wav")]
+        [InlineData(1305, "digits/1.wav!digits/thousand.wav!digits/3.wav!digits/hundred.wav!digits/5.wav")]
+        [InlineData(1310, "digits/1.wav!digits/thousand.wav!digits/3.wav!digits/hundred.wav!digits/10.wav")]
+        [InlineData(2316, "digits/2.wav!digits/thousand.wav!digits/3.wav!digits/hundred.wav!digits/16.wav")]
+        [InlineData(2323, "digits/2.wav!digits/thousand.wav!digits/3.wav!digits/hundred.wav!digits/20.wav!digits/3.wav")]
+        [InlineData(2399, "digits/2.wav!digits/thousand.wav!digits/3.wav!digits/hundred.wav!digits/90.wav!digits/9.wav")]
+        [InlineData(20009, "digits/20.wav!digits/thousand.wav!digits/9.wav")]
+        [InlineData(21239, "digits/20.wav!digits/1.wav!digits/thousand.wav!digits/2.wav!digits/hundred.wav!digits/30.wav!digits/9.wav")]
+        [InlineData(123456, "digits/1.wav!digits/hundred.wav!digits/20.wav!digits/3.wav!digits/thousand.wav!digits/4.wav!digits/hundred.wav!digits/50.wav!digits/6.wav")]
+        [InlineData(999999, "digits/9.wav!digits/hundred.wav!digits/90.wav!digits/9.wav!digits/thousand.wav!digits/9.wav!digits/hundred.wav!digits/90.wav!digits/9.wav")]
+        [InlineData(2123456, "digits/2.wav!digits/million.wav!digits/1.wav!digits/hundred.wav!digits/20.wav!digits/3.wav!digits/thousand.wav!digits/4.wav!digits/hundred.wav!digits/50.wav!digits/6.wav")]
+        [InlineData(9999999, "digits/9.wav!digits/million.wav!digits/9.wav!digits/hundred.wav!digits/90.wav!digits/9.wav!digits/thousand.wav!digits/9.wav!digits/hundred.wav!digits/90.wav!digits/9.wav")]
+        [InlineData(1000023, "digits/1.wav!digits/million.wav!digits/20.wav!digits/3.wav")]
+        [InlineData(123000000, "digits/1.wav!digits/hundred.wav!digits/20.wav!digits/3.wav!digits/million.wav")]
+        public void can_convert_digits_to_file_strings(int input, string expectedOutput)
+        {
+            var output = Digits.ToFileString(input);
+            output.Should().Be(
+                expectedOutput, 
+                "\nexpected: '{0}'\nactual: '{1}'\n".Fmt(
+                    expectedOutput.Replace("digits/","").Replace(".wav","")
+                    ,output.Replace("digits/", "").Replace(".wav", "")));
         }
     }
 }
