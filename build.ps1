e85592c063b29896dda016fce2b11259971783d4
Dan Barua <danbarua@gmail.com>
2016-06-30 11:55:35 +0100

    Fix build

--- build.ps1
+++ build.ps1
@@ -13,11 +13,11 @@ if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 $packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -eq "packages.config"}
 foreach($packageConfig in $packageConfigs){
   Write-Host "Restoring" $packageConfig.FullName
-  .\.nuget\nuget.exe i $packageConfig.FullName -o .\packages
+  nuget.exe i $packageConfig.FullName -o .\packages
 }
 
 Import-Module .\packages\psake.4.4.1\tools\psake.psm1
 Import-Module .\BuildFunctions.psm1
-Invoke-Psake .\default.ps1 $task -framework "4.0x64" -properties @{ buildNumber=$buildNumber }
+Invoke-Psake .\default.ps1 $task -framework "4.5.1" -properties @{ buildNumber=$buildNumber }
 Remove-Module BuildFunctions
 Remove-Module psake
